77c3d9e7570a9f2d8278327dc886500787515ef5
dojo-se <dojo-se@wagner-laptop.(none)>
2011-03-17 18:23:23 -0300

    Passou test_altura_1 (Rodrigo)

--- triangulo_pascal.py
+++ triangulo_pascal.py
@@ -1,11 +1,11 @@
 import unittest2 as unittest
 
-def problema_para_resolver(parametro):
-    return 1 == parametro
+def triangulo_de_pascal(parametro):
+    return '1'
 
-class TestSequenceFunctions(unittest.TestCase):
-    def test_simples(self):
-        self.assertEqual(True, problema_para_resolver(1))
+class TrianguloTestCase(unittest.TestCase):
+    def test_altura_1(self):
+        self.assertEqual('1', triangulo_de_pascal(1))
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
