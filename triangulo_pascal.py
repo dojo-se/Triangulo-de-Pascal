866763c4bd7c4c43c52e23b3375a2c22e82871d0
dojo-se <dojo-se@wagner-laptop.(none)>
2011-03-17 18:35:48 -0300

    Altura 2 (Ricardo e Luiz)

--- triangulo_pascal.py
+++ triangulo_pascal.py
@@ -1,11 +1,18 @@
 import unittest2 as unittest
 
 def triangulo_de_pascal(parametro):
-    return '1'
+    if parametro == 1:
+       r = '1'
+    elif parametro == 2:
+       r = ' 1\n1 1'
+    return r
 
 class TrianguloTestCase(unittest.TestCase):
     def test_altura_1(self):
         self.assertEqual('1', triangulo_de_pascal(1))
+        
+    def test_altura_2(self):
+        self.assertEqual(' 1\n1 1', triangulo_de_pascal(2))
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
