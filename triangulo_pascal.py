28c946f2303516fba77a997c8b9ed339829b9c35
dojo-se <dojo-se@wagner-laptop.(none)>
2011-03-17 20:16:27 -0300

    Davi indocil (Davi)

--- triangulo_pascal.py
+++ triangulo_pascal.py
@@ -1,23 +1,36 @@
-import unittest2 as unittest
+import unittest2
 
 def _cria_matriz_do_triangulo(altura):
    dimensoes = range(altura)
    base = range((2 * altura) - 1)
    return [[0 for x in base] for x in dimensoes]
+
+def _le_matriz(matriz, linha, coluna):
+    try:
+        return matriz[linha][coluna]
+    except IndexError:
+        return 0
+
+def _povoa_matriz(matriz):
+    altura = len(matriz)
+    largura = len(matriz[0])
+    matriz[0][altura-1] = 1
+    for i in range(1, altura):
+        for j in range(largura):
+            valorAnte = _le_matriz(matriz, i-1, j-1)
+            valorProx = _le_matriz(matriz, i-1, j+1)
+            matriz[i][j] = valorAnte + valorProx
+    return matriz
    
 def _formata_matriz_do_triangulo(matriz):
-    matriz[0][0] = 1
     return str(matriz[0][0])
 
 def triangulo_de_pascal(altura):
-    if altura == 1:
-        matriz = _cria_matriz_do_triangulo(1)
-        r = _formata_matriz_do_triangulo(matriz)
-    elif altura == 2:
-        r = ' 1\n1 1'
-    return r
+    matriz = _cria_matriz_do_triangulo(altura)
+    matriz_povoada = _povoa_matriz(matriz)
+    return _formata_matriz_do_triangulo(matriz_povoada)
 
-class TrianguloTestCase(unittest.TestCase):
+class TrianguloTestCase(unittest2.TestCase):
     def test_altura_1(self):
         self.assertEqual('1', triangulo_de_pascal(1))
         
@@ -25,4 +38,4 @@ class TrianguloTestCase(unittest.TestCase):
         self.assertEqual(' 1\n1 1', triangulo_de_pascal(2))
 
 if __name__ == '__main__':
-    unittest.main(verbosity=2)
+    unittest2.main(verbosity=2)
